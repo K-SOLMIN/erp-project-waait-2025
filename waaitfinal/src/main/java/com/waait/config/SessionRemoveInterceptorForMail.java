2ec345905e2b31916577d90d1f4140e6684b4169
solmi <solmi@DESKTOP-SIEVIDO>
2025-09-20 20:57:31 +0900

    <sm> interceptor 클래스 component로 등록

--- waaitfinal/src/main/java/com/waait/config/SessionRemoveInterceptorForMail.java
+++ waaitfinal/src/main/java/com/waait/config/SessionRemoveInterceptorForMail.java
@@ -1,11 +1,13 @@
 package com.waait.config;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+@Component
 public class SessionRemoveInterceptorForMail implements HandlerInterceptor{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
