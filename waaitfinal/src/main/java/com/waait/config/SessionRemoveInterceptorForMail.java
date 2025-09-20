83cd298436ec087137527d69b15e3f42096ee98c
solmi <solmi@DESKTOP-SIEVIDO>
2025-09-20 20:04:28 +0900

    <sm> 선택했던 메일함으로 돌아가기위한 interceptor클래스 추가

---
+++ waaitfinal/src/main/java/com/waait/config/SessionRemoveInterceptorForMail.java
@@ -0,0 +1,20 @@
+package com.waait.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class SessionRemoveInterceptorForMail implements HandlerInterceptor{
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		String uri = request.getRequestURI();
+		HttpSession session = request.getSession(false);
+		
+		if(!uri.contains("/mail") && session != null) session.removeAttribute("selectedMailBox");
+		
+		return true;
+	}
+}
