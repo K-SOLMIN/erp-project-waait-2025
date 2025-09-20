c6f642defa4c207c7d8baf540987578d0562f22e
solmi <solmi@DESKTOP-SIEVIDO>
2025-09-20 21:11:20 +0900

    <sm> urlpattern수정밑 확인용 syso삭제

--- waaitfinal/src/main/java/com/waait/config/SessionRemoveInterceptorForMail.java
+++ waaitfinal/src/main/java/com/waait/config/SessionRemoveInterceptorForMail.java
@@ -15,7 +15,6 @@ public class SessionRemoveInterceptorForMail implements HandlerInterceptor{
 		String uri = request.getRequestURI();
 		HttpSession session = request.getSession(false);
 		
-		System.out.println("interceptort실행");
 		if(!uri.contains("/mail") && session != null) session.removeAttribute("selectedMailBox");
 		
 		return true;
--- waaitfinal/src/main/java/com/waait/config/WebConfig.java
+++ waaitfinal/src/main/java/com/waait/config/WebConfig.java
@@ -15,7 +15,8 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		
-		registry.addInterceptor(sessionRemoveInterceptor).excludePathPatterns("/mail/**");
+		registry.addInterceptor(sessionRemoveInterceptor)
+			.excludePathPatterns("/mail/**","/resources/**","/app-assets/**");
 
 	}
 	/*
