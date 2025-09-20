5fd924c3509314ad4a427e22cb1d17d7b062f2a5
solmi <solmi@DESKTOP-SIEVIDO>
2025-09-20 21:00:29 +0900

    <sm> new연산자로 객체생성시 오류 가능성이있어 requiredArgsConstrutor로 주입하는 방식으로 변경

--- waaitfinal/src/main/java/com/waait/config/WebConfig.java
+++ waaitfinal/src/main/java/com/waait/config/WebConfig.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+	
+	private final SessionRemoveInterceptorForMail sessionRemoveInterceptor;
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new SessionRemoveInterceptorForMail()).excludePathPatterns("/mail/**");
+		
+		registry.addInterceptor(sessionRemoveInterceptor).excludePathPatterns("/mail/**");
 
 	}
 	/*
