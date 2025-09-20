fdcd42b80de47f59ea021c98f3c0ba29d8ea046e
solmi <solmi@DESKTOP-SIEVIDO>
2025-09-20 20:35:15 +0900

    <sm> configuration annotation add

--- waaitfinal/src/main/java/com/waait/config/WebConfig.java
+++ waaitfinal/src/main/java/com/waait/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.waait.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 	
 	@Override
