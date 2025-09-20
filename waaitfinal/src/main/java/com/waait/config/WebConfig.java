2f29a40e93f6ae665325780b2f15a66d7c3d2bab
solmi <solmi@DESKTOP-SIEVIDO>
2025-09-20 20:31:01 +0900

    <sm> interceptor 적용을위해 webConfiguration클래스에 addInterceptors메소드 Override

--- waaitfinal/src/main/java/com/waait/config/WebConfig.java
+++ waaitfinal/src/main/java/com/waait/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.waait.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
