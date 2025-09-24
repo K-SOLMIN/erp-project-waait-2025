806b8d7cb864108e2dfff991871164446848229d
solmi <solmi@DESKTOP-SIEVIDO>
2025-09-24 21:29:08 +0900

    <sm>interceptor exclude urlpattern 추가

--- waaitfinal/src/main/java/com/waait/config/WebConfig.java
+++ waaitfinal/src/main/java/com/waait/config/WebConfig.java
@@ -16,7 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		
 		registry.addInterceptor(sessionRemoveInterceptor)
-			.excludePathPatterns("/mail/**","/resources/**","/app-assets/**");
+			.excludePathPatterns("/mail/**", "/resources/**",  "/app-assets/**"
+									, "/assets/**", "/.well-known/**", "/error");
 
 	}
 	/*
